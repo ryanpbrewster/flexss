9b0aad90fa2131679b9edf47250c4bc7a6cbc9e5
Ryan P. Brewster <RyanPBrewster@gmail.com>
2024-01-22 21:05:09 -0500

    Benchmark rendevouz_shuffle

---
+++ benches/my_benchmark.rs
@@ -0,0 +1,23 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use flexss::{rendevouz_shuffle::RendevouzShuffle, BackendId, Health, Picker, TenantId};
+
+fn rendevouz_shuffle(c: &mut Criterion) {
+    let mut p = RendevouzShuffle::new(100);
+    let backends: Vec<BackendId> = (0..1_000).map(BackendId).collect();
+    for &b in &backends {
+        p.register(b, Health::Up);
+    }
+    let tenant_id = TenantId(0);
+    c.bench_function("rendevous_shuffle_pick", |b| {
+        b.iter(|| {
+            black_box(p.pick(tenant_id))
+        })
+    });
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default();
+    targets = rendevouz_shuffle,
+}
+criterion_main!(benches);
